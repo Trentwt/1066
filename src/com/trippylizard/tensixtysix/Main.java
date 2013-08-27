f66b7f2f270c5b0652f24578e2bd6348532f1c29
Trent <trent_thompson@rogers.com>
2013-08-27 19:46:12 -0400

    Some keyboard bindings added

--- src/com/trippylizard/tensixtysix/Main.java
+++ src/com/trippylizard/tensixtysix/Main.java
@@ -6,7 +6,9 @@ import static org.lwjgl.opengl.GL11.*;
 import java.io.BufferedInputStream;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.openal.AL;
+import org.lwjgl.openal.AL10;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.util.WaveData;
@@ -15,6 +17,11 @@ public class Main {
 
 	int WIDTH = 1280;
 	int HEIGHT = 768;
+	
+	int albuffer;
+	int menuthemesource;
+	
+	boolean musicon = true;
 	
 	public Main() {
 		try {
@@ -34,22 +41,33 @@ public class Main {
 		glOrtho(0, WIDTH, HEIGHT, 0, 1, -1);
 		glMatrixMode(GL_MODELVIEW);
 		
-		BufferedInputStream stream = new BufferedInputStream(this.getClass().getResourceAsStream("theme.wav"));
-		WaveData data = WaveData.create(stream);
-		System.out.println(data == null);
-		int buffer = alGenBuffers();
-		alBufferData(buffer, data.format, data.data, data.samplerate);
-		data.dispose();
-		int source = alGenSources();
-		
-		alSourcef(source, AL_BUFFER, buffer);
-		
-		alSourcePlay(source);
+		playMenuMusic();
 		
 		while (!Display.isCloseRequested()) {
 			//Render Code
 			
 			glClear(GL_COLOR_BUFFER_BIT);
+			
+			if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+				alDeleteBuffers(albuffer);
+				AL.destroy();
+				Display.destroy();
+				System.exit(0);
+			}
+			
+			while (Keyboard.next()) {
+				if (Keyboard.isKeyDown(Keyboard.KEY_F10)) {
+					if (Keyboard.getEventKeyState()) {
+						if (musicon) {
+							alSourcePause(menuthemesource);
+							musicon = false;
+						} else {
+							AL10.alSourcePlay(menuthemesource);
+							musicon = true;
+						}
+					}
+				}
+			}
 			
 			glBegin(GL_TRIANGLES);
 				glVertex2i(100, 100);
@@ -61,13 +79,27 @@ public class Main {
 			Display.sync(120);
 		}
 		
-		alDeleteBuffers(buffer);
+		alDeleteBuffers(albuffer);
 		AL.destroy();
 		Display.destroy();
+		System.exit(0);
 	}
 	
 	public static void main(String[] args) {
 		new Main();
+	}
+	
+	private void playMenuMusic() {
+		BufferedInputStream stream = new BufferedInputStream(this.getClass().getResourceAsStream("theme.wav"));
+		WaveData data = WaveData.create(stream);
+		albuffer = alGenBuffers();
+		alBufferData(albuffer, data.format, data.data, data.samplerate);
+		data.dispose();
+		menuthemesource = alGenSources();
+		
+		alSourcef(menuthemesource, AL_BUFFER, albuffer);
+		
+		alSourcePlay(menuthemesource);
 	}
 
 }
