dbdc5670cb2877ba7303438d5ea9743b3f317e58
Trent <trent_thompson@rogers.com>
2013-08-27 11:00:13 -0400

    Render Test Code

--- src/com/trippylizard/tensixtysix/Main.java
+++ src/com/trippylizard/tensixtysix/Main.java
@@ -1,9 +1,48 @@
 package com.trippylizard.tensixtysix;
 
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.*;
+
 public class Main {
 
-	public static void main(String[] args) {
+	int WIDTH = 1280;
+	int HEIGHT = 768;
+	
+	public Main() {
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.setTitle("1066 - Alpha Version 0.0.1");
+			Display.create();
+		} catch (LWJGLException ex) {
+			ex.printStackTrace();
+		}
 		
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, WIDTH, HEIGHT, 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
+		
+		while (!Display.isCloseRequested()) {
+			//Render Code
+			
+			glClear(GL_COLOR_BUFFER_BIT);
+			
+			glBegin(GL_TRIANGLES);
+				glVertex2i(100, 100);
+				glVertex2i(WIDTH - 100, 100);
+				glVertex2i(HEIGHT - 100, WIDTH / 2);
+			glEnd();
+			
+			Display.update();
+			Display.sync(120);
+		}
+		
+		Display.destroy();
+	}
+	
+	public static void main(String[] args) {
+		new Main();
 	}
 
 }
