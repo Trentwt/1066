d0637c2599b65fc6ea46db3603eea97ffc9eef20
Trent <trent_thompson@rogers.com>
2013-08-28 15:53:42 -0400

    Fixed more bugs that I forgot to fix in the last commit

--- src/com/trippylizard/tensixtysix/Main.java
+++ src/com/trippylizard/tensixtysix/Main.java
@@ -1,34 +1,7 @@
 package com.trippylizard.tensixtysix;
 
-import static org.lwjgl.openal.AL10.AL_BUFFER;
-import static org.lwjgl.openal.AL10.alBufferData;
-import static org.lwjgl.openal.AL10.alDeleteBuffers;
-import static org.lwjgl.openal.AL10.alGenBuffers;
-import static org.lwjgl.openal.AL10.alGenSources;
-import static org.lwjgl.openal.AL10.alSourcePause;
-import static org.lwjgl.openal.AL10.alSourcePlay;
-import static org.lwjgl.openal.AL10.alSourcef;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_COMPILE;
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
-import static org.lwjgl.opengl.GL11.GL_PROJECTION;
-import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
-import static org.lwjgl.opengl.GL11.GL_VERSION;
-import static org.lwjgl.opengl.GL11.glBegin;
-import static org.lwjgl.opengl.GL11.glCallList;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glDeleteLists;
-import static org.lwjgl.opengl.GL11.glEnd;
-import static org.lwjgl.opengl.GL11.glEndList;
-import static org.lwjgl.opengl.GL11.glGenLists;
-import static org.lwjgl.opengl.GL11.glGetString;
-import static org.lwjgl.opengl.GL11.glLoadIdentity;
-import static org.lwjgl.opengl.GL11.glMatrixMode;
-import static org.lwjgl.opengl.GL11.glNewList;
-import static org.lwjgl.opengl.GL11.glNormal3f;
-import static org.lwjgl.opengl.GL11.glOrtho;
-import static org.lwjgl.opengl.GL11.glVertex2i;
-import static org.lwjgl.opengl.GL11.glVertex3f;
+import static org.lwjgl.openal.AL10.*;
+import static org.lwjgl.opengl.GL11.*;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -48,9 +21,7 @@ import org.lwjgl.util.vector.Vector3f;
 import org.newdawn.slick.util.ResourceLoader;
 
 import com.trippylizard.tensixtysix.fighter.Fighter;
-import com.trippylizard.tensixtysix.models.Face;
-import com.trippylizard.tensixtysix.models.Model;
-import com.trippylizard.tensixtysix.models.OBJModelLoader;
+import com.trippylizard.tensixtysix.models.*;
 import com.trippylizard.tensixtysix.nations.Normans;
 import com.trippylizard.tensixtysix.utils.StreamUtils;
 
